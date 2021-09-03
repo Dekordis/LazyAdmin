bfa1c68143450fe82fa4791186f50feb61edf6b5
Dekordis <matp@ciklum.com>
2021-09-03 17:14:15 +0300

    Create Azure.cs

---
+++ LazyAdmin/LazyAdmin/Scripts/Azure.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LazyAdmin
+{
+    partial class App
+    {
+        //We must use this .CS for all methods which work with Azure 
+        static void AzureConnection()//This method needed for connect and disconnect Azure Folder
+        {
+
+        }
+    }
+}
