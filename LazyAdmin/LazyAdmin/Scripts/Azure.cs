dd7feb03e93f746ed539373540eca9f17adae1d1
SkhoCiklum <skho@ciklum.com>
2021-09-07 11:24:17 +0300

    Add connecting Azure

--- LazyAdmin/LazyAdmin/Scripts/Azure.cs
+++ LazyAdmin/LazyAdmin/Scripts/Azure.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 
 namespace LazyAdmin
 {
@@ -9,8 +10,13 @@ namespace LazyAdmin
         string User; //This variable needed for contain User of Azure
         string MainPath; //This variable contain Path of programm
         string RemotePath; ////This variable contain Remote Path to Azure folder 
-        private void AzureConnection()//This method needed for connect and disconnect Azure Folder
+        public void AzureConnection()//This method needed for connect and disconnect Azure Folder
         {
+            string StringCmdText;
+            StringCmdText = @"/C cmdkey /add:cklfsstorage.file.core.windows.net /user:Azure\cklfsstorage /pass:NSLF6WgwRXvwHdNQxvEjZWsCMvXrsBts3H+nqnAdycFNkxROKF6U3z9MP7/Lz+kluXfjdO+ewwcISduwQlKWgA== >nul 2>&1" + "\n" + @"net use L: \\cklfsstorage.file.core.windows.net\remoteinstall /persistent:No >nul 2>&1";
+            System.Diagnostics.Process.Start("CMD.exe", StringCmdText);
+            MessageBox.Show("Connected");
+
             //Dear Serhii please create method with 2 parameters (connect, disconnect) Azure disk 
             //When method was finished it must delete all temp files and other not necessary objects (!just only use disconnect parametr!) 
         }
