f7dc25d565d13eb4ef38d8c5ba0a0039639b242c
Dekordis <matp@ciklum.com>
2021-09-15 14:43:02 +0300

    Update NavTool.cs

--- LazyAdmin/LazyAdmin/Scripts/NavTool.cs
+++ LazyAdmin/LazyAdmin/Scripts/NavTool.cs
@@ -4,6 +4,25 @@ namespace LazyAdmin
 {
     partial class App
     {
+        static public void Upload()
+        {
 
+        }
+        static public void Input()
+        {
+
+        }
+        static private void Cheking()
+        {
+
+        }
+        static public void Checkboxes()
+        {
+
+        }
+        static public void Output()
+        {
+
+        }
     }
 }
